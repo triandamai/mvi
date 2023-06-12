880981610550bb2f4138009de497b706ac061c0e
trian damai <triannurizkillah@gmail.com>
2023-06-12 14:59:48 +0700

    feat: modularize app

--- app/src/main/java/app/trian/resepku/MainActivity.kt
+++ app/src/main/java/app/trian/resepku/MainActivity.kt
@@ -3,8 +3,14 @@ package app.trian.resepku
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.runtime.LaunchedEffect
 import androidx.navigation.compose.NavHost
 import app.trian.core.ui.BaseMainApp
+import app.trian.core.ui.UIController
+import app.trian.core.ui.extensions.addOnEventListener
+import app.trian.core.ui.listener.AppStateEventListener
+import app.trian.core.ui.listener.ScreenToAppEvent
+import app.trian.core.ui.rememberUIController
 import app.trian.core.ui.routes.AuthenticationConstants
 import app.trian.resepku.feature.authentication.authenticationRoute
 import app.trian.resepku.feature.dashboard.dashboardRoute
@@ -13,10 +19,21 @@ import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
+    private lateinit var uiController: UIController
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            BaseMainApp {
+            uiController = rememberUIController()
+            LaunchedEffect(key1 = uiController, block = {
+                uiController.addOnEventListener { event ->
+                    when (event) {
+                        ScreenToAppEvent.EXIT_APP -> finish()
+                    }
+                }
+            })
+            BaseMainApp(
+                controller = uiController
+            ) {
                 NavHost(
                     navController = it.router,
                     startDestination = AuthenticationConstants.parentRoute
