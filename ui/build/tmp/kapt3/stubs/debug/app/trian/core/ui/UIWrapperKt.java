8b25e38add9a5411aac133e3f14c0a115157a26c
trian damai <triannurizkillah@gmail.com>
2023-06-16 23:55:46 +0700

    feat: add ksp

--- ui/build/tmp/kapt3/stubs/debug/app/trian/core/ui/UIWrapperKt.java
+++
@@ -1,7 +0,0 @@
-package app.trian.core.ui;
-
-import java.lang.System;
-
-@kotlin.Metadata(mv = {1, 8, 0}, k = 2, d1 = {"\u0000 \n\u0000\n\u0002\u0010\u0002\n\u0000\n\u0002\u0018\u0002\n\u0002\b\u0002\n\u0002\u0018\u0002\n\u0002\u0018\u0002\n\u0002\u0018\u0002\n\u0002\b\u0002\u001aK\u0010\u0000\u001a\u00020\u0001\"\u0012\b\u0000\u0010\u0002\u0018\u0001*\n\u0012\u0002\b\u0003\u0012\u0002\b\u00030\u00032\u0006\u0010\u0004\u001a\u0002H\u00022\u001c\u0010\u0005\u001a\u0018\u0012\u0004\u0012\u0002H\u0002\u0012\u0004\u0012\u00020\u00010\u0006\u00a2\u0006\u0002\b\u0007\u00a2\u0006\u0002\b\bH\u0087\b\u00f8\u0001\u0000\u00a2\u0006\u0002\u0010\t\u0082\u0002\u0007\n\u0005\b\u009920\u0001\u00a8\u0006\n"}, d2 = {"UIWrapper", "", "T", "Lapp/trian/core/ui/UIListener;", "uiEvent", "content", "Lkotlin/Function1;", "Landroidx/compose/runtime/Composable;", "Lkotlin/ExtensionFunctionType;", "(Lapp/trian/core/ui/UIListener;Lkotlin/jvm/functions/Function1;)V", "core-ui_debug"})
-public final class UIWrapperKt {
-}
--- ui/build/tmp/kapt3/stubs/debug/app/trian/core/ui/listener/BottomSheetListener.java
+++
@@ -1,9 +0,0 @@
-package app.trian.core.ui.listener;
-
-import java.lang.System;
-
-@kotlin.Metadata(mv = {1, 8, 0}, k = 1, d1 = {"\u0000\u0016\n\u0002\u0018\u0002\n\u0002\u0010\u0000\n\u0000\n\u0002\u0010\u0002\n\u0000\n\u0002\u0010\u000b\n\u0000\b\u00e6\u0080\u0001\u0018\u00002\u00020\u0001J\u0010\u0010\u0002\u001a\u00020\u00032\u0006\u0010\u0004\u001a\u00020\u0005H&\u00a8\u0006\u0006"}, d2 = {"Lapp/trian/core/ui/listener/BottomSheetListener;", "", "showBottomSheet", "", "isShow", "", "core-ui_debug"})
-public abstract interface BottomSheetListener {
-    
-    public abstract void showBottomSheet(boolean isShow);
-}
