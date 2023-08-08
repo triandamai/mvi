0cd75ea5f15983794e689718f8e66b50f8894f55
trian damai <triannurizkillah@gmail.com>
2023-08-08 08:26:05 +0700

    feat: updating internal

--- ui/src/main/java/app/trian/mvi/ui/internal/ToastMvi.kt
+++ ui/src/main/java/app/trian/mvi/ui/internal/ToastMvi.kt
@@ -8,8 +8,12 @@ import app.trian.mvi.ui.internal.listener.ToastListener
 class ToastMvi(
     private val context:Context
 ):ToastListener {
-    override fun show(message: String, length: Int) {
-        Toast.makeText(context,message,length).show()
+    override fun show(message: String) {
+        Toast.makeText(context,message,Toast.LENGTH_SHORT).show()
+    }
+
+    override fun show(message: Int, vararg params: Any) {
+        Toast.makeText(context,context.getString(message,params),Toast.LENGTH_SHORT).show()
     }
 
 }
